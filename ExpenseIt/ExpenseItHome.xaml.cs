d125caf2f66e536d6860c5c681497b6af3caba6c
jagadeeshgoftx <jagadeesh@goftx.com>
2018-03-22 15:05:38 -0400

    added connection to database populating table data

--- ExpenseIt/ExpenseItHome.xaml.cs
+++ ExpenseIt/ExpenseItHome.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Configuration;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace ExpenseIt
 {
@@ -23,12 +26,29 @@ namespace ExpenseIt
         public ExpenseItHome()
         {
             InitializeComponent();
+            BindData();
+        }
+
+        private void BindData()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["connectexpenses"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select name from Expenses";
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable("Expenses");
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+
+            peopleListBox.ItemsSource = dt.DefaultView;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             // View Expense Report  
-            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
             this.NavigationService.Navigate(expenseReportPage);
         }
     }
